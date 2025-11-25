a42cefe424998b5f50d1f389fb1a715635ecbc84
Annika Shankwitz <annikashankwitz@Zelda-7.local>
2025-11-25 14:09:53 -0500

    slice from midpoint

--- cochleogramTest/testCochlegram.py
+++ cochleogramTest/testCochlegram.py
@@ -20,7 +20,7 @@ def extract(path, startInterval, endInterval, window=30):
     
     audio, sr = librosa.load(path, sr=None)
  
-    #root mean squared normalization
+    #loudness normalization
     meter = pyln.Meter(sr)
     loudness = meter.integrated_loudness(audio)
     loud_norm_audio = pyln.normalize.loudness(audio, loudness, -25.0)
@@ -28,31 +28,61 @@ def extract(path, startInterval, endInterval, window=30):
     #slice
     #get the duration in MS
     duration = (endInterval-startInterval)
-    print(f'duration:{duration}')
+    print(f'duration:{duration} ms')
 
-    #figure out, in ms, where the 1/3 point of the vowel is
-    startMS = startInterval + (duration/3)
-    print(f'startMs:{startMS}')
+    #get the midpoint in ms
+    #and find in it frames
+    midpoint = startInterval + duration/2
+    midpointFrame = int(midpoint * (sr/1000))
+    print(f'midpoint:{midpoint} ms')
+    print(f'midpointFrame: {midpointFrame}')
 
-    #figure out how many frames will be in a 30 ms window based on the sr
-    framesInSlice = sr * (window/1000)
-    print(f'framesInSlice:{framesInSlice}')
+    #figure out how many frames will be in half the window
+    framesInSlice = sr * ((window/2)/1000)
+    print(f'framesInSlice:{framesInSlice} frames')
 
-    #convert the starting point to a frame index
-    startFrame = int(startMS * (sr/1000))
-    print(f'startFrame:{startFrame}')
+    #find the start and the end frame
+    #15 ms off the mid point in either direction
+    startFrame = int(midpointFrame-framesInSlice)
+    print(f"startFrame:{startFrame}")
+    endFrame = int(midpointFrame+framesInSlice)
+    print(f"endFrame: {endFrame}")
 
-    #find the ending index
-    endFrame = int(startFrame+framesInSlice)
-    print(f'endFrame:{endFrame}')
-
-    #find the ending time in ms
+    #find the end and the start in ms
     endMS = endFrame/(sr/1000)
-    print(f'endMS:{endMS}')
+    startMS = startFrame/(sr/1000)
+    print(f"startMS:{startMS}")
+    print(f"endMS:{endMS}")
 
     # check that the slice didn't exceed the vowel length
-    if endInterval < endMS:
+    if endMS > endInterval or startInterval > startMS:
         raise ValueError(f"Attemped to slice. Vowel not long enough")
+
+    #from old version
+    #figure out, in ms, where the 1/3 point of the vowel is
+    # startMS = startInterval + (duration/3)
+    # print(f'startMs:{startMS}')
+
+    # #figure out how many frames will be in a 30 ms window based on the sr
+    # framesInSlice = sr * (window/1000)
+    # print(f'framesInSlice:{framesInSlice}')
+
+    # #convert the starting point to a frame index
+    # startFrame = int(startMS * (sr/1000))
+    # print(f'startFrame:{startFrame}')
+
+    #find the ending index
+    # endFrame = int(startFrame+framesInSlice)
+    # print(f'endFrame:{endFrame}')
+
+    #find the ending time in ms
+    # endMS = endFrame/(sr/1000)
+    # print(f'endMS:{endMS}')
+
+    # check that the slice didn't exceed the vowel length
+    #if endInterval < endMS:
+        #raise ValueError(f"Attemped to slice. Vowel not long enough")
+
 
     segment = loud_norm_audio[startFrame:endFrame]
 
@@ -69,7 +99,7 @@ def extract(path, startInterval, endInterval, window=30):
 
     #average over the time dimension
     cg_column = np.mean(cg, axis=1)
-    np.savetxt('440Hz.csv', cg_column, delimiter=',')
+    #np.savetxt('440Hz.csv', cg_column, delimiter=',')
     return(cg_column)
 
 if __name__ == "__main__":
